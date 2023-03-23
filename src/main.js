cd5559e6b3272fc3037ad45eeb736a8a06226cc4
576 <double@double-cash.com>
2023-03-23 22:33:02 +0800

    新增swiper

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-import './index.css'
+import "./index.css";
+
+import Swiper, { Navigation, Pagination } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination],
+});
