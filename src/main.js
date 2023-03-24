f08345d09854dc56fbed6369809957da206bbc55
576 <double@double-cash.com>
2023-03-24 22:20:56 +0800

    手機版完成

--- src/main.js
+++ src/main.js
@@ -12,4 +12,8 @@ const swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
